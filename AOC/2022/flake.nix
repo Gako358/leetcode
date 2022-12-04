5006a04612b731060987f2063f1190ddd6e0b3c5
Gako358 <gako358@outlook.com>
2022-12-04 23:52:45 +0100

    setting up flake for aoc 2022

---
+++ AOC/2022/flake.nix
@@ -0,0 +1,70 @@
+{
+  inputs = {
+    nixpkgs.url = "github:jordanisaacs/nixpkgs/aoc-cli-init";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crate2nix = {
+      url = "github:kolloch/crate2nix";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    crate2nix,
+    ...
+  }: let
+    system = "x86_64-linux";
+    overlays = [
+      rust-overlay.overlays.default
+      (self: super: let
+        rust = super.rust-bin.stable.latest.default;
+      in {
+        rustc = rust;
+        cargo = rust;
+      })
+    ];
+
+    pkgs = import nixpkgs {
+      inherit system overlays;
+    };
+    inherit
+      (import "${crate2nix}/tools.nix" {inherit pkgs;})
+      generateCargoNix
+      ;
+
+    name = "advent-of-code";
+    pkg =
+      (
+        import
+        (generateCargoNix {
+          inherit name;
+          src = ./.;
+        })
+        {inherit pkgs;}
+      )
+      .workspaceMembers
+      .client
+      .build;
+
+    nativeBuildInputs = with pkgs; [
+      rustc
+      cargo
+      cargo-edit
+      cargo-audit
+      cargo-tarpaulin
+      clippy
+    ];
+  in
+    with pkgs; {
+      packages.${system} = {
+        "${name}" = pkg;
+        default = pkg;
+      };
+
+      devShells.${system}.default = mkShell {
+        nativeBuildInputs = nativeBuildInputs ++ [pkgs.aoc-cli];
+      };
+    };
+}
